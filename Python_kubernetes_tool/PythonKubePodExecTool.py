a6b55b1fd5b80f70308ff3590d984de18ac86ce1
panzershaft <contractor.soham@gmail.com>
2021-01-30 23:34:04 +0000

    Added colour, made more user-readable cli

--- Python_kubernetes_tool/PythonKubePodExecTool.py
+++ Python_kubernetes_tool/PythonKubePodExecTool.py
@@ -8,6 +8,7 @@ import subprocess
 
 from colorama import Fore, Back, Style
 from termcolor import colored, cprint
+from tabulate import tabulate
 
 
 class PythonKubePodExecTool(object):
@@ -15,54 +16,69 @@ class PythonKubePodExecTool(object):
     def __init__(self):
         self.config = config.load_kube_config()
         self.v1 = client.CoreV1Api()
+        self.pod_list = []
 
     def list_all_pods_all_namespaces(self):
         print("\nListing all the pods\n")
-        print("\033[1;32;40m \n")
         data = self.v1.list_pod_for_all_namespaces(watch=False)
         for i in data.items:
-            print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+            self.pod_list.append([i.status.pod_ip, i.metadata.namespace, i.metadata.name])
+        print(tabulate(self.pod_list, headers=['IP', 'NAMESPACE', 'POD_NAME']))
+        return True
+
+    def list_available_pods_namespaces(self):
+        print("\nSELECT FROM THE FOLLOWING PODS, NAMESPACES")
+        self.list_all_pods_all_namespaces()
         return True
 
     def exec_in_pod(self, pod=None, namespace="default", usr_command="echo this is an echo "
                                                                      "from the pod"):
+        """
 
-        exec_command = ['/bin/sh']
-        resp = stream(self.v1.connect_get_namespaced_pod_exec,
-                      pod,
-                      namespace,
-                      command=exec_command,
-                      stderr=True, stdin=True,
-                      stdout=True, tty=False,
-                      _preload_content=False)
+        :param pod: name of pod (str)
+        :param namespace: string value
+        :param usr_command: unix command
+        :return: True
+        """
+        try:
+            exec_command = ['/bin/sh']
+            resp = stream(self.v1.connect_get_namespaced_pod_exec,
+                          pod,
+                          namespace,
+                          command=exec_command,
+                          stderr=True, stdin=True,
+                          stdout=True, tty=False,
+                          _preload_content=False)
 
-        commands = [
-            usr_command
-        ]
-        data_items = []
-        print("\033[1;32;40m \n")
-        while resp.is_open():
-            resp.update(timeout=1)
-            if resp.peek_stdout():
-                print("STDOUT:\n")
-                print("%s" % resp.read_stdout())
-                data_items.append(resp.read_stdout if resp.read_stdout else None)
-            if resp.peek_stderr():
-                print("STDERR: %s" % resp.read_stderr())
-            if commands:
-                c = commands.pop(0)
-                print("Running command: %s\n" % c)
-                resp.write_stdin(c + "\n")
-            else:
-                break
+            commands = [
+                usr_command
+            ]
 
-        resp.write_stdin("date\n")
-        print("date: %s" % resp.read_stdout())
-        resp.write_stdin("whoami\n")
-        print("user: %s" % resp.read_stdout())
+            while resp.is_open():
+                resp.update(timeout=1)
+                if resp.peek_stdout():
+                    print("STDOUT:\n")
+                    print("%s" % resp.read_stdout())
+                if resp.peek_stderr():
+                    print("STDERR: %s" % resp.read_stderr())
+                if commands:
+                    c = commands.pop(0)
+                    print("Running command: %s\n" % c)
+                    resp.write_stdin(c + "\n")
+                else:
+                    break
 
-        resp.close()
-        return resp
+            resp.write_stdin("date\n")
+            print("date: %s" % resp.read_stdout())
+
+            resp.write_stdin("whoami\n")
+            print("user: %s" % resp.read_stdout())
+
+            resp.close()
+            return resp
+
+        except Exception as e:
+            return "Exception caught"
 
 
 def main():
@@ -73,25 +89,33 @@ def main():
     subprocess.call(script, shell=True)
     obj = PythonKubePodExecTool()
 
-    print_white_on_red = lambda x: cprint(x, 'white', 'on_red')
+    print_red_on_white = lambda x: cprint(x, 'red', 'on_white')
 
     while True:
         print("\n")
-        print_white_on_red('########################################################')
-        print_white_on_red('#######   SELECT ANY OF THE FOLLOWING OPTION   #########')
-        print_white_on_red('########################################################')
+        print_red_on_white('########################################################')
+        print_red_on_white('#######   SELECT ANY OF THE FOLLOWING OPTION   #########')
+        print_red_on_white('########################################################')
         print("\n")
-        cprint("1. TO SEE ALL THE PODS", 'blue', attrs=['bold'], file=sys.stderr)
-        cprint("2. TO EXEC INTO A POD", 'blue', attrs=['bold'], file=sys.stderr)
+
+        cprint("1. List all pods of all namespaces", 'blue', attrs=['bold'], file=sys.stderr)
+        cprint("2. Exec into a particular pod", 'blue', attrs=['bold'], file=sys.stderr)
         cprint("0. TO EXIT", 'blue', attrs=['bold'], file=sys.stderr)
+
         num = input("\nEnter number: ")
+
         if num == '1':
             obj.list_all_pods_all_namespaces()
+
         if num == '2':
-            cmd = input("\nEnter the command you wish to execute: ")
+            obj.list_available_pods_namespaces()
+            pod_name = input("\nEnter the pod to exec: ")
+            cmd = input("\nEnter the command to exec: ")
+
             if not cmd:
                 cmd = "python --version"
-            obj.exec_in_pod("api-pod", "default", cmd)
+            obj.exec_in_pod(pod_name, "default", cmd)
+
         if num == '0':
             break
 
